583f40f6bbc267a93bbfabca925a2df7cb09a1f0
Pantomik <addr@domain.ext>
2021-05-13 20:31:25 +0200

    Pizza orderring and Tests

--- sources/pizza.py
+++ sources/pizza.py
@@ -9,10 +9,6 @@ RECIPES = {
         'price': 10,
         'sauce': 'tomato'
     },
-    'marinara': {
-        'ingredients': {'garlic': 2, 'basil': 3},
-        'price': 9,
-    },
     'quattro fromagi': {
         'ingredients': {'mozzarella': 2, 'parmesan': 2, 'gorgonzola': 2, 'cheddar': 2, 'oregano': 3},
         'price': 12,
@@ -28,22 +24,4 @@ RECIPES = {
         'price': 11,
         'sauce': 'tomato'
     },
-    'romana': {
-        'ingredients': {'ham': 4, 'olive': 3, 'onion': 2, 'basil': 2},
-        'price': 12,
-    },
-    'original': {
-        'ingredients': {'garlic': 5, 'pepper': 4, 'parmesan': 2},
-        'price': 10,
-        'sauce': 'cream'
-    },
-    'ultima': {
-        'ingredients': {'gorgonzola': 4, 'pepper': 5, 'onion': 4, 'ham': 3},
-        'price': 12,
-        'sauce': 'cream'
-    },
-    'mediterranea': {
-        'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
-        'price': 11,
-    }
 }
