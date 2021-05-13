14ac5047984d4a22943d593ac13c9a9f148b3dd5
Pantomik <addr@domain.ext>
2021-05-13 20:29:02 +0200

    Adding sauce management

--- sources/pizza.py
+++ sources/pizza.py
@@ -1,8 +1,13 @@
 
 RECIPES = {
+        'my pizza': {
+        'ingredients': {'mozzarella': 3, 'garlic': 5},
+        'price':10
+    },
     'margherita': {
         'ingredients': {'mozzarella': 2, 'oregano': 4},
         'price': 10,
+        'sauce': 'tomato'
     },
     'marinara': {
         'ingredients': {'garlic': 2, 'basil': 3},
@@ -11,14 +16,17 @@ RECIPES = {
     'quattro fromagi': {
         'ingredients': {'mozzarella': 2, 'parmesan': 2, 'gorgonzola': 2, 'cheddar': 2, 'oregano': 3},
         'price': 12,
+        'sauce': 'tomato'
     },
     'crudo': {
         'ingredients': {'mozzarella': 2, 'ham': 3, 'basil': 3, 'mushroom': 3},
         'price': 11,
+        'sauce': 'tomato'
     },
     'pugliese': {
         'ingredients': {'mozzarella': 4, 'oregano': 2, 'onion': 5, 'mushroom': 2},
         'price': 11,
+        'sauce': 'tomato'
     },
     'romana': {
         'ingredients': {'ham': 4, 'olive': 3, 'onion': 2, 'basil': 2},
@@ -27,17 +35,15 @@ RECIPES = {
     'original': {
         'ingredients': {'garlic': 5, 'pepper': 4, 'parmesan': 2},
         'price': 10,
+        'sauce': 'cream'
     },
     'ultima': {
         'ingredients': {'gorgonzola': 4, 'pepper': 5, 'onion': 4, 'ham': 3},
         'price': 12,
+        'sauce': 'cream'
     },
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    },
-    'my pizza': {
-        'ingredients': {'mozzarella': 3, 'garlic': 5},
-        'price':10
     }
 }
